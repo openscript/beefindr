179e6d01b2f019addd631c72d1a07dab003be40d
Robin <1105080+openscript@users.noreply.github.com>
2019-05-03 20:44:36 +0200

    Enable create the app test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,12 +23,13 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-/*   it('should create the app', () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
+/*
   it(`should have as title 'beefinder'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
