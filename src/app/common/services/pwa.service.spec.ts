cf8bf0e8d6a278a1e003875d2a1b9d6386bb3a25
DimiTs <dtsichlakis@yahoo.de>
2019-05-11 14:44:49 +0200

    Test Adjustment PWA

--- src/app/common/services/pwa.service.spec.ts
+++ src/app/common/services/pwa.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { PwaService } from './pwa.service';
+import { environment } from '../../../environments/environment';
 
 describe('PwaService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ]}));
 
   it('should be created', () => {
     const service: PwaService = TestBed.get(PwaService);
