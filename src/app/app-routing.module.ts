3313cad14c4feba71f02c45759f085a22147a2b6
andreas.schorro-indrawati@students.ffhs.ch <andreas.schorro-indrawati@students.ffhs.ch>
2019-05-15 00:33:38 +0200

    Finished Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StartPageComponent} from "./views/start-page/start-page.component";
+import {AddReportComponent} from "./views/add-report/add-report.component";
+import {RegisterUserComponent} from "./views/register-user/register-user.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/start-page', pathMatch: 'full' },
+  { path: 'start-page', component: StartPageComponent },
+  { path: 'add-report', component: AddReportComponent },
+  { path: 'register-user', component: RegisterUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
