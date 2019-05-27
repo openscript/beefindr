a036e58f17597bc007f9068b06cc8caf9b317930
Samuel Blattner <samworks@gmx.net>
2019-05-27 08:45:08 +0200

    #32 #33 – Implement skeleton for claim/decline mechanism

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,6 +5,7 @@ import {HiveNotifier} from "./notification/hiveNotifier";
 import {LogDispatcher} from "./notification/dispatchers/log/log.dispatcher";
 import {MailDispatcher} from "./notification/dispatchers/email/mail.dispatcher";
 import {MessagingDispatcher} from "./notification/dispatchers/firebase-cloud-messaging/messaging.dispatcher";
+import {HiveManager} from "./beehive/hiveManager";
 
 
 // // Start writing Firebase Functions
@@ -20,3 +21,47 @@ export const handleNewBeehive = functions.firestore.document('beehive/{uid}').on
   const notifier: HiveNotifier = new HiveNotifier([new LogDispatcher(), new MailDispatcher(), new MessagingDispatcher()]);
   return notifier.notifyClosestBeekeeper(new BeeHive((<SerializedBeeHive>{id: snap.id, ...snap.data()})));
 });
+
+/**
+ * Fulfills the claim on a hive by a BeeKeeper.
+ * A hive-token is required to have access to the hive.
+ */
+export const claimBeehive = functions.https.onRequest(async (req, res) => {
+
+    const token = req.query.token;
+
+    if (!token) {
+      res.status(400).send('No hive token provided or unable to parse token!');
+      return;
+    }
+
+    try {
+      (new HiveManager).claimHive(token);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+    res.status(200).send();
+  }
+);
+
+/**
+ * Fulfills the claim on a hive by a BeeKeeper.
+ * A hive-token is required to have access to the hive.
+ */
+export const declineBeehive = functions.https.onRequest(async (req, res) => {
+
+    const token = req.query.token;
+
+    if (!token) {
+      res.status(400).send('No hive token provided or unable to parse token!');
+      return;
+    }
+
+    try {
+      (new HiveManager).declineHive(token);
+    } catch (e) {
+      res.status(400).send(e);
+    }
+    res.status(200).send();
+  }
+);
