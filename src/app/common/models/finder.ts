93402c6530e372f20d2e7661fae1e2d1a0f74368
Robin <1105080+openscript@users.noreply.github.com>
2019-05-30 00:21:41 +0200

    Remove model default implementations

--- src/app/common/models/finder.ts
+++ src/app/common/models/finder.ts
@@ -1,13 +1,7 @@
-import { Address, defaultAddress } from './address';
+import { Address } from './address';
 
 export interface Finder {
   name: string;
   address: Address;
   email: string;
 }
-
-export const defaultFinder: Finder = {
-  name: undefined,
-  address: defaultAddress,
-  email: undefined
-};
